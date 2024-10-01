5541b003b61578026a4c219d9b16f02037ac3ebf
dpqlsn <a01037111209@gmail.com>
2024-10-02 01:26:28 +0900

    FIX :: 임의의 데이터들을 표에 넣음

--- gomdorilo/src/App.js
+++ gomdorilo/src/App.js
@@ -79,29 +79,31 @@ function App() {
       </div>
 
       <div className="line"></div>
-
       <div className="posts">
-        <table>
+        <table className="styled-table">
           <thead>
             <tr>
-              <th>no</th> <th>제목</th> <th>작성자</th> <th>작성일</th> <th>추천</th>
+              <th>no</th>
+              <th className="title-column">제목</th>
+              <th className="author-column">작성자</th>
+              <th className="date-column">작성일</th>
+              <th>추천</th>
             </tr>
           </thead>
           <tbody>
             {filteredPosts.map((post, index) => (
-              <tr key={index}> 
+              <tr key={index}>
               <td>{post.no}</td>
-                <td>{post.title}</td>
-                <td>{post.author}</td>
-                <td>{post.date}</td>
-                <td>{post.recommendations}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+              <td className="title-column">{post.title}</td>
+              <td className="author-column">{post.author}</td>
+              <td className="date-column">{post.date}</td>
+              <td>{post.recommendations}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
-}
+  </div>
+);}
 
 export default App;
