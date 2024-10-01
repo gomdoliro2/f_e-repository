053200752d23fc64dec0f667528c5f523b4f8c07
dpqlsn <a01037111209@gmail.com>
2024-10-01 23:22:28 +0900

    ADD : 로고, 검색바 구현

--- gomdorilo/src/App.js
+++ gomdorilo/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import picture1 from './img/material-symbols_search.png';
+import picture2 from './img/frame.png';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('Jin_venus08');
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [posts, setPosts] = useState([
+  ]);
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  const handleSearch = () => {
+    const results = posts.filter(post => post.includes(searchTerm));
+    setFilteredPosts(results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <p>BamGallary</p>
+        </div>
+        <div className="search">
+          <input id="search-bar" type="text" placeholder="검색어를 입력하세요" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+          <img src={picture1} alt="search-symbol" className="search-icon" onClick={handleSearch} />
+        </div>
+        <div className="mypost">
+          <p>내 게시물 <span> | </span> {username} </p>
+          <img src={picture2} alt="face-symbol" className="face-icon" />
+        </div>
+      </div>
+      <div className="posts">
+        <ul>
+          {filteredPosts.map((post, index) => (
+            <li key={index}>{post}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
