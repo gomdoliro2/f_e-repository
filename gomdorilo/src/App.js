49a059e092a0f6777651766a329e01552802ffd2
dpqlsn <a01037111209@gmail.com>
2024-10-02 00:22:47 +0900

    ADD :: 새 글 추가 버튼

--- gomdorilo/src/App.js
+++ gomdorilo/src/App.js
@@ -48,21 +48,14 @@ function App() {
       </div>
       <div className="type">
         <div className="type-item" onClick={() => handleTypeClick('작성일')}>
-          <p id="date" style={{ color: selectedType === '작성일' ? '#000' : '#666' }}>
-            작성일
-          </p>
+          <p id="date" style={{ color: selectedType === '작성일' ? '#000' : '#666' }}> 작성일 </p>
           {selectedType === '작성일' && <div className="underline" />}
         </div>
         <div className="type-item" onClick={() => handleTypeClick('인기글')}>
-          <p id="fame" style={{ color: selectedType === '인기글' ? '#000' : '#666' }}>
-            인기글
-          </p>
-          {selectedType === '인기글' && <div className="underline" />}
-        </div>
+          <p id="fame" style={{ color: selectedType === '인기글' ? '#000' : '#666' }}> 인기글 </p>
+          {selectedType === '인기글' && <div className="underline" />}</div>
         <div className="type-item" onClick={() => handleTypeClick('팔로우')}>
-          <p id="follow" style={{ color: selectedType === '팔로우' ? '#000' : '#666' }}>
-            팔로우
-          </p>
+          <p id="follow" style={{ color: selectedType === '팔로우' ? '#000' : '#666' }}> 팔로우 </p>
           {selectedType === '팔로우' && <div className="underline" />}
         </div>
         <button className="new">새 글 작성</button>
