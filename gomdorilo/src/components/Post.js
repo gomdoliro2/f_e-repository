7ed4e79f0ac54b6730bd1c56ad5e58fc7232db20
dpqlsn <a01037111209@gmail.com>
2024-10-03 15:23:02 +0900

    ADD :: 추천수 댓글수

--- gomdorilo/src/components/Post.js
+++ gomdorilo/src/components/Post.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom'; 
 import picture2 from '../img/frame.png';
 
 const Post = () => {
     const location = useLocation();
-    const { title, content} = location.state || {};
+    const navigate = useNavigate();
+    const { title = '', content = '' } = location.state || {};
+
+    const [titleState] = useState(title);
+    const [contentState] = useState(content);
+
+    const handleEdit = () => {
+        navigate('/new-post', {
+            state: { title: titleState, content: contentState },
+        });
+    };
+
+    const handleDelete = () => {
+        navigate('/');
+    };
 
     return (
         <div className="post-container">
@@ -25,12 +39,43 @@ const Post = () => {
                     <img src={picture2} alt="face-symbol" className="face-icon" />
                 </div>
             </div>
-            <div className="post-content">
-                <h1>{title}</h1> 
-                <p>{content}</p>
+            <div className="post-form">
+                <div className="show">
+                    <input
+                        type="text"
+                        value={titleState}
+                        readOnly
+                        className="title-show"
+                    />
+                        <button onClick={handleEdit} id="edit">수정</button>
+                        <button onClick={handleDelete} id="delete">삭제</button>
+                </div>
+                <div className="thin-ubderline" />
+                <textarea
+                    value={contentState}
+                    readOnly
+                    className="textarea"
+                />
+                <div className="thin-line" />
+                <div className="show">
+                    <div className="good">
+                        추천
+                        <div className="margin">
+                            <span id="num">0</span>
+                        </div>
+                    </div>
+                    <div className="comments">
+                        댓글
+                        <div className="margin">
+                            <span id="num">0</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="thin-line" />
             </div>
         </div>
     );
-};
+    
+}    
 
 export default Post;
